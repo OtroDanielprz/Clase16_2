0b447aff98b7a9f8fccabc73fcf2d2628074a8fe
carab <carab@LAPTOP-NCRIVJ1F>
2022-11-16 12:42:45 -0700

    Ejemplo de la clase 16_2

---
+++ src/clase16_2/Clase16_2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clase16_2;
+
+/**
+ *
+ * @author carab
+ */
+public class Clase16_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
